0ab393aedf78c91358ced419817b49599c6632ad
Kenneth Ascheri <kascheri12@gmail.com>
2022-08-29 23:58:25 -0700

    KenBox_csharp initial code commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using KenBox_csharp.Models;
+
+namespace KenBox_csharp.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            var watch = new System.Diagnostics.Stopwatch();
+
+            watch.Start();
+            int count_recur = new KenBox(new KenBox(new KenBox(new KenBox(new KenBox(new KenBox(new KenBox(new KenBox(new KenBox(new KenBox(new KenBox(new KenBox(new KenBox(new KenBox(new KenBox(new KenBox(new KenBox(new KenBox(new KenBox(new KenBox(new KenBox(new KenBox(new KenBox(new KenBox(new KenBox(new KenBox(new KenBox(new KenBox(new KenBox(new KenBox(new KenBox(new KenBox(new KenBox(new KenBox(new KenBox(new KenBox(new KenBox(new KenBox(new KenBox(new KenBox(new KenBox(new KenBox(new KenBox(new KenBox(new KenBox(new KenBox(new KenBox(new KenBox(new KenBox(new KenBox(new KenBox(new KenBox(new KenBox(new KenBox(new KenBox(new KenBox(new KenBox(new KenBox(new KenBox(new KenBox()))))))))))))))))))))))))))))))))))))))))))))))))))))))))))).NumberOfContainedBoxesRecursive;
+            watch.Stop();
+            long ticks_recur = watch.ElapsedTicks;
+
+            watch.Restart();
+            int count_iter = new KenBox(new KenBox(new KenBox(new KenBox(new KenBox(new KenBox(new KenBox(new KenBox(new KenBox(new KenBox(new KenBox(new KenBox(new KenBox(new KenBox(new KenBox(new KenBox(new KenBox(new KenBox(new KenBox(new KenBox(new KenBox(new KenBox(new KenBox(new KenBox(new KenBox(new KenBox(new KenBox(new KenBox(new KenBox(new KenBox(new KenBox(new KenBox(new KenBox(new KenBox(new KenBox(new KenBox(new KenBox(new KenBox(new KenBox(new KenBox(new KenBox(new KenBox(new KenBox(new KenBox(new KenBox(new KenBox(new KenBox(new KenBox(new KenBox(new KenBox(new KenBox(new KenBox(new KenBox(new KenBox(new KenBox(new KenBox(new KenBox(new KenBox(new KenBox(new KenBox()))))))))))))))))))))))))))))))))))))))))))))))))))))))))))).NumberOfContainedBoxesIterative;
+            watch.Stop();
+            long ticks_iter = watch.ElapsedTicks;
+
+            long perc_faster = (ticks_recur - ticks_iter) / ticks_iter * 100;
+
+            ViewBag.CountRecur = count_recur;
+            ViewBag.CountIter = count_iter;
+            ViewBag.TicksRecur = ticks_recur;
+            ViewBag.TicksIter = ticks_iter;
+            ViewBag.PercFaster = perc_faster;
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
+
---
+++ KenBox.cs
@@ -0,0 +1,49 @@
+﻿using System;
+namespace KenBox_csharp
+{
+    public class KenBox
+    {
+        private readonly KenBox containedBox;
+
+        public KenBox() { }
+
+        public KenBox(KenBox box)
+        {
+            this.containedBox = box;
+        }
+
+        public int getNumberOfChildBoxesRecurse(KenBox box)
+        {
+            if (box.containedBox is KenBox)
+            {
+                return getNumberOfChildBoxesRecurse(box.containedBox) + 1;
+            }
+            return 0;
+        }
+
+        public int NumberOfContainedBoxesRecursive
+        {
+            get
+            {
+                return getNumberOfChildBoxesRecurse(this);
+            }
+        }
+
+        public int NumberOfContainedBoxesIterative
+        {
+            get
+            {
+                int i = 0;
+                KenBox child = this.containedBox;
+                while (child is KenBox)
+                {
+                    i++;
+                    if (child.containedBox is KenBox) { child = child.containedBox;  }
+                    else { child = null; }
+                }
+                return i;
+            }
+        }
+    }
+}
+
